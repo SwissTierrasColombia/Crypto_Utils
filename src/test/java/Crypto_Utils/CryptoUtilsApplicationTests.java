53ddde750f39e9aba7ada08f52ca4eb15d16c973
MauricioAcosta <amacostapulido@gmail.com>
2020-04-30 11:11:12 -0500

    add jar

---
+++ src/test/java/Crypto_Utils/CryptoUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package Crypto_Utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
